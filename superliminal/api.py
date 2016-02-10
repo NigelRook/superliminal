928c98266f11a5c1f6db7e81d12710e14f881e5c
Nigel Rook <nigel@electricimp.com>
2016-02-10 19:38:01 +0000

    CouchPotato handler

--- superliminal/api.py
+++ superliminal/api.py
@@ -2,9 +2,10 @@ import logging
 import os.path
 from tornado.web import RequestHandler, Application
 from tornado.ioloop import IOLoop
-from tornado.escape import json_decode
+from tornado.escape import json_decode, url_escape
 from tornado.httpserver import HTTPServer
 from tornado.httpclient import HTTPClient, HTTPRequest
+from tornado.httputil import parse_body_arguments
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +20,32 @@ class AddHandler(RequestHandler):
         logger.info("ADD: %s -> %s", path, name)
         with self._core:
             self._core.add_video(path, name)
+
+
+class CouchPotatoHandler(RequestHandler):
+    def initialize(self, core_factory):
+        self._core = core_factory.get()
+
+    def post(self):
+        id = self.request.body_arguments['imdb_id']
+
+        http_client = HTTPClient()
+        request = HTTPRequest(
+            method='GET',
+            url='%s/api/%s/media.get?id=%d' %
+                (self._core.settings.couchpotato_url,
+                 self,_core.settings.couchpotato_api_key,
+                 url_escape(id)))
+        response = http_client.fetch(request)
+        movie_data = json_decode(response.body)
+        release = next((release for release in movie_data['media']['releases'] if release['status'] == 'done'))
+        path = release['files']['movie'][0]
+        name = release['info']['name']+os.path.splitext(path)[1]
+        logger.info("ADD (couchpotato): %s -> %s", path, name)
+
+        with self._core:
+            self._core.add_video(path, name)
+
 
 class SonarrHandler(RequestHandler):
     def initialize(self, core_factory):
@@ -58,6 +85,7 @@ def run(core_factory):
     init_params = { 'core_factory': core_factory }
     routes = [
         ('/add', AddHandler, init_params),
+        ('/add/couchpotato', CouchPotatoHandler, init_params),
         ('/add/sonarr', SonarrHandler, init_params)
     ]
     application = Application(routes)
