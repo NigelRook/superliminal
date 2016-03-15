73d25c324dff044ac254f938c7970f5281484cbc
Nigel Rook <nigel@electricimp.com>
2016-03-15 20:37:36 +0000

    More get_downloads_for_video tests

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -43,3 +43,22 @@ def test_added_sub_is_returned(fixture):
     downloads = datastore.get_downloads_for_video(path)
 
     assert downloads == {lang: [{'provider': provider, 'sub_id': sub_id, 'lang': lang, 'score': score}]}
+
+def test_added_subs_are_returned_by_lang(fixture):
+    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
+    path = "/data/Series/Season 1/01 Title.mkv"
+    video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
+    datastore.add_video(path, video)
+
+    provider1 = "davessubs"; sub_id1 = "ABC123"; lang1 = Language.fromietf('en'); score1 = 123
+    provider2 = "stevesubs"; sub_id2 = "steve123"; lang2 = lang1; score2 = 120
+    provider3 = "pablosubs"; sub_id3 = "umdoistres"; lang3 = Language.fromietf('pt-BR'); score3 = 150
+    datastore.add_download(path, provider1, sub_id1, lang1, score1)
+    datastore.add_download(path, provider2, sub_id2, lang2, score2)
+    datastore.add_download(path, provider3, sub_id3, lang3, score3)
+
+    downloads = datastore.get_downloads_for_video(path)
+
+    assert downloads == {lang1: [{'provider': provider1, 'sub_id': sub_id1, 'lang': lang1, 'score': score1},
+                                 {'provider': provider2, 'sub_id': sub_id2, 'lang': lang2, 'score': score2}],
+                         lang3: [{'provider': provider3, 'sub_id': sub_id3, 'lang': lang3, 'score': score3}]}
