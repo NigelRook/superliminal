aa39e911ca7fdadaf47a9ec2dc8cf072f2069b87
Nigel Rook <nigel@electricimp.com>
2016-03-15 20:40:42 +0000

    test code tidyup

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -34,10 +34,7 @@ def test_added_sub_is_returned(fixture):
     path = "/data/Series/Season 1/01 Title.mkv"
     video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
     datastore.add_video(path, video)
-    provider = "davessubs"
-    sub_id = "ABC123"
-    lang = Language.fromietf('en')
-    score = 123
+    provider = "davessubs"; sub_id = "ABC123"; lang = Language.fromietf('en'); score = 123
     datastore.add_download(path, provider, sub_id, lang, score)
 
     downloads = datastore.get_downloads_for_video(path)
