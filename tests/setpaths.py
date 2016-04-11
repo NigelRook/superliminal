194c2b25b25b902574e674c555959aa5b2195240
Nigel Rook <nigelrook@gmail.com>
2016-04-11 15:45:19 +0100

    Ditch pytest, use unittest

---
+++ tests/setpaths.py
@@ -0,0 +1,4 @@
+import sys, os
+base_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(base_path, '..'))
+sys.path.insert(0, os.path.join(base_path, '../libs'))
--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -1,174 +1,164 @@
+import setpaths
 import pytest
 import tempfile
 from subliminal.video import Video
 from babelfish import Language
 from datetime import datetime, timedelta
 
-import superliminal.datastore
+from superliminal.datastore import SqLiteDataStore
 
-class DatastoreFixture:
-    def __init__(self):
+from unittest import TestCase
+from nose_parameterized import parameterized
+
+class DataStoreTests(TestCase):
+    def setUp(self):
         self.db_file = tempfile.NamedTemporaryFile()
-        self.db_path = self.db_file.name
+        self.datastore = SqLiteDataStore(self.db_file.name)
 
-    def finalize(self):
+    def tearDown(self):
+        self.datastore.close()
         self.db_file.close()
 
-@pytest.fixture
-def fixture(request):
-    f = DatastoreFixture()
-    request.addfinalizer(f.finalize)
-    return f
+    def test_newly_added_video_has_no_downloads(self):
+        path = "/data/Series/Season 1/01 Title.mkv"
+        video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
+        self.datastore.add_video(path, video)
 
-def test_newly_added_video_has_no_downloads(fixture):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
-    path = "/data/Series/Season 1/01 Title.mkv"
-    video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
-    datastore.add_video(path, video)
+        downloads = self.datastore.get_downloads_for_video(path)
 
-    downloads = datastore.get_downloads_for_video(path)
+        self.assertEqual(downloads, {})
 
-    assert downloads == {}
+    def test_added_sub_is_returned(self):
+        path = "/data/Series/Season 1/01 Title.mkv"
+        video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
+        self.datastore.add_video(path, video)
+        provider = "davessubs"; sub_id = "ABC123"; lang = Language.fromietf('en'); score = 123
+        self.datastore.add_download(path, provider, sub_id, lang, score)
 
-def test_added_sub_is_returned(fixture):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
-    path = "/data/Series/Season 1/01 Title.mkv"
-    video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
-    datastore.add_video(path, video)
-    provider = "davessubs"; sub_id = "ABC123"; lang = Language.fromietf('en'); score = 123
-    datastore.add_download(path, provider, sub_id, lang, score)
+        downloads = self.datastore.get_downloads_for_video(path)
 
-    downloads = datastore.get_downloads_for_video(path)
+        self.assertEqual(downloads, {lang: [{'provider': provider, 'sub_id': sub_id, 'lang': lang, 'score': score}]})
 
-    assert downloads == {lang: [{'provider': provider, 'sub_id': sub_id, 'lang': lang, 'score': score}]}
+    def test_added_subs_are_returned_by_lang(self):
+        path = "/data/Series/Season 1/01 Title.mkv"
+        video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
+        self.datastore.add_video(path, video)
 
-def test_added_subs_are_returned_by_lang(fixture):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
-    path = "/data/Series/Season 1/01 Title.mkv"
-    video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
-    datastore.add_video(path, video)
+        provider1 = "davessubs"; sub_id1 = "ABC123"; lang1 = Language.fromietf('en'); score1 = 123
+        provider2 = "stevesubs"; sub_id2 = "steve123"; lang2 = lang1; score2 = 120
+        provider3 = "pablosubs"; sub_id3 = "umdoistres"; lang3 = Language.fromietf('pt-BR'); score3 = 150
+        self.datastore.add_download(path, provider1, sub_id1, lang1, score1)
+        self.datastore.add_download(path, provider2, sub_id2, lang2, score2)
+        self.datastore.add_download(path, provider3, sub_id3, lang3, score3)
 
-    provider1 = "davessubs"; sub_id1 = "ABC123"; lang1 = Language.fromietf('en'); score1 = 123
-    provider2 = "stevesubs"; sub_id2 = "steve123"; lang2 = lang1; score2 = 120
-    provider3 = "pablosubs"; sub_id3 = "umdoistres"; lang3 = Language.fromietf('pt-BR'); score3 = 150
-    datastore.add_download(path, provider1, sub_id1, lang1, score1)
-    datastore.add_download(path, provider2, sub_id2, lang2, score2)
-    datastore.add_download(path, provider3, sub_id3, lang3, score3)
+        downloads = self.datastore.get_downloads_for_video(path)
 
-    downloads = datastore.get_downloads_for_video(path)
+        self.assertEqual(downloads, {lang1: [{'provider': provider1, 'sub_id': sub_id1, 'lang': lang1, 'score': score1},
+                                             {'provider': provider2, 'sub_id': sub_id2, 'lang': lang2, 'score': score2}],
+                                     lang3: [{'provider': provider3, 'sub_id': sub_id3, 'lang': lang3, 'score': score3}]})
 
-    assert downloads == {lang1: [{'provider': provider1, 'sub_id': sub_id1, 'lang': lang1, 'score': score1},
-                                 {'provider': provider2, 'sub_id': sub_id2, 'lang': lang2, 'score': score2}],
-                         lang3: [{'provider': provider3, 'sub_id': sub_id3, 'lang': lang3, 'score': score3}]}
+    def test_replacing_video_for_path_removes_downloads(self):
+        path = "/data/Series/Season 1/01 Title.mkv"
+        video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
+        self.datastore.add_video(path, video)
+        provider = "davessubs"; sub_id = "ABC123"; lang = Language.fromietf('en'); score = 123
+        self.datastore.add_download(path, provider, sub_id, lang, score)
 
-def test_replacing_video_for_path_removes_downloads(fixture):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
-    path = "/data/Series/Season 1/01 Title.mkv"
-    video = Video.fromname("Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv")
-    datastore.add_video(path, video)
-    provider = "davessubs"; sub_id = "ABC123"; lang = Language.fromietf('en'); score = 123
-    datastore.add_download(path, provider, sub_id, lang, score)
+        video2 = Video.fromname("Series.S01E02.Title.720p.BDRip-ReleaseGroup2.mkv")
+        self.datastore.add_video(path, video)
+        downloads = self.datastore.get_downloads_for_video(path)
 
-    video2 = Video.fromname("Series.S01E02.Title.720p.BDRip-ReleaseGroup2.mkv")
-    datastore.add_video(path, video)
-    downloads = datastore.get_downloads_for_video(path)
+        self.assertEqual(downloads, {})
 
-    assert downloads == {}
+    def test_get_incomplete_videos_returns_nothing_when_no_videos_exist(self):
+        lang = Language.fromietf('en')
+        incomplete = self.datastore.get_incomplete_videos([lang], 100, 100, datetime.utcnow()- timedelta(days=1))
 
-def test_get_incomplete_videos_returns_nothing_when_no_videos_exist(fixture):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
+    @parameterized.expand([
+        ('no videos returns empty', [], []),
+        ('video with no downloads returns all languages',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[]}],
+         [(0, [('en', 0), ('pt-BR', 0)])]),
+        ('excludes languages with downloads >= desired',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100)]}],
+         [(0, [('pt-BR', 0)])]),
+        ('excludes video when all languages have downloads >= desired',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100),
+                        ('pablosubs', 'ps16', 'pt-BR', 123)]}],
+         []),
+        ('includes languages with downloads < desired',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100),
+                        ('pablosubs', 'ps16', 'pt-BR', 80)]}],
+         [(0, [('pt-BR', 80)])]),
+        ('returns highest score for language when multiple downloads found',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100),
+                        ('pablosubs', 'ps16', 'pt-BR', 83),
+                        ('pablosubs', 'ps22', 'pt-BR', 80)]}],
+         [(0, [('pt-BR', 83)])]),
+        ('excludes language if any score >= desired',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100),
+                        ('stevesubs', 'steve69', 'en', 83),
+                        ('pablosubs', 'ps22', 'pt-BR', 80)]}],
+         [(0, [('pt-BR', 80)])]),
+        ('uses different desired score for movies',
+         [{'path':"/data/Title (2016)/Title.mkv",
+           'name':"Title.2016.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 80),
+                        ('pablosubs', 'ps16', 'pt-BR', 79)]}],
+         [(0, [('pt-BR', 79)])]),
+        ('returns multiple videos',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('pablosubs', 'ps16', 'pt-BR', 80)]},
+          {'path':"/data/Series/Season 1/03 Title.mkv",
+           'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 100)]}],
+         [(0, [('en', 0), ('pt-BR', 80)]),
+          (1, [('pt-BR', 0)])]),
+        ('returns multiple videos',
+         [{'path':"/data/Series/Season 1/02 Title.mkv",
+           'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[],
+           'age':timedelta(days=2)},
+          {'path':"/data/Series/Season 1/03 Title.mkv",
+           'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[('davesubs', 'ds123', 'en', 80)],
+           'age':timedelta(days=2)},
+          {'path':"/data/Series/Season 1/03 Title.mkv",
+           'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
+           'downloads':[],
+           'age':timedelta(hours=23)}],
+         [(2, [('en', 0), ('pt-BR', 0)])])
+    ], testcase_func_name=lambda func, num, param: '%s_%s'%(
+        func.__name__, parameterized.to_safe_name(param.args[0])
+    ))
+    def test_get_incomplete_videos(self, name, videos, expected):
+        for video in videos:
+            added = datetime.utcnow() - video['age'] if 'age' in video else timedelta(0)
+            self.datastore.add_video(video['path'], Video.fromname(video['name']), added)
+            for provider, sub_id, lang, score in video['downloads']:
+                self.datastore.add_download(video['path'], provider, sub_id, Language.fromietf(lang), score)
 
-    lang = Language.fromietf('en')
-    incomplete = datastore.get_incomplete_videos([lang], 100, 100, datetime.utcnow()- timedelta(days=1))
+        lang1 = Language.fromietf('en'); lang2 = Language.fromietf('pt-BR')
+        incomplete = self.datastore.get_incomplete_videos([lang1, lang2], 80, 100, datetime.utcnow() - timedelta(days=1))
 
-get_incomplete_videos_scenarios = [
-    {'name':'no videos returns empty',
-     'videos':[],
-     'expected':[]},
-    {'name':'video with no downloads returns all languages',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[]}],
-     'expected':[(0, [('en', 0), ('pt-BR', 0)])]},
-    {'name':'excludes languages with downloads >= desired',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100)]}],
-     'expected':[(0, [('pt-BR', 0)])]},
-    {'name':'excludes video when all languages have downloads >= desired',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100),
-                             ('pablosubs', 'ps16', 'pt-BR', 123)]}],
-     'expected':[]},
-    {'name':'includes languages with downloads < desired',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100),
-                             ('pablosubs', 'ps16', 'pt-BR', 80)]}],
-     'expected':[(0, [('pt-BR', 80)])]},
-    {'name':'returns highest score for language when multiple downloads found',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100),
-                             ('pablosubs', 'ps16', 'pt-BR', 83),
-                             ('pablosubs', 'ps22', 'pt-BR', 80)]}],
-     'expected':[(0, [('pt-BR', 83)])]},
-    {'name':'excludes language if any score >= desired',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100),
-                             ('stevesubs', 'steve69', 'en', 83),
-                             ('pablosubs', 'ps22', 'pt-BR', 80)]}],
-     'expected':[(0, [('pt-BR', 80)])]},
-    {'name':'uses different desired score for movies',
-     'videos':[{'path':"/data/Title (2016)/Title.mkv",
-                'name':"Title.2016.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 80),
-                             ('pablosubs', 'ps16', 'pt-BR', 79)]}],
-     'expected':[(0, [('pt-BR', 79)])]},
-    {'name':'returns multiple videos',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('pablosubs', 'ps16', 'pt-BR', 80)]},
-               {'path':"/data/Series/Season 1/03 Title.mkv",
-                'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 100)]}],
-     'expected':[(0, [('en', 0), ('pt-BR', 80)]),
-                 (1, [('pt-BR', 0)])]},
-    {'name':'returns multiple videos',
-     'videos':[{'path':"/data/Series/Season 1/02 Title.mkv",
-                'name':"Series.S01E02.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[],
-                'age':timedelta(days=2)},
-               {'path':"/data/Series/Season 1/03 Title.mkv",
-                'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[('davesubs', 'ds123', 'en', 80)],
-                'age':timedelta(days=2)},
-               {'path':"/data/Series/Season 1/03 Title.mkv",
-                'name':"Series.S01E03.Title.720p.WEB-DL.DD5.1.H264-ReleaseGroup.mkv",
-                'downloads':[],
-                'age':timedelta(hours=23)}],
-     'expected':[(2, [('en', 0), ('pt-BR', 0)])]}
-]
-
-@pytest.mark.parametrize("scenario", get_incomplete_videos_scenarios, ids=lambda scenario:scenario['name'])
-def test_get_incomplete_videos(fixture, scenario):
-    datastore = superliminal.datastore.SqLiteDataStore(fixture.db_path)
-    for video in scenario['videos']:
-        added = datetime.utcnow() - video['age'] if 'age' in video else timedelta(0)
-        datastore.add_video(video['path'], Video.fromname(video['name']), added)
-        for provider, sub_id, lang, score in video['downloads']:
-            datastore.add_download(video['path'], provider, sub_id, Language.fromietf(lang), score)
-
-    lang1 = Language.fromietf('en'); lang2 = Language.fromietf('pt-BR')
-    incomplete = datastore.get_incomplete_videos([lang1, lang2], 80, 100, datetime.utcnow() - timedelta(days=1))
-
-    assert len(incomplete) == len(scenario['expected'])
-    for video_idx, needs in scenario['expected']:
-        actual = next(video for video in incomplete if video['path'] == scenario['videos'][video_idx]['path'])
-        assert actual['video'].name == scenario['videos'][video_idx]['name']
-        assert len(actual['needs']) == len(needs)
-        for lang, score in needs:
-            actual_need = next(need for need in actual['needs'] if str(need['lang']) == lang)
-            assert actual_need['current_score'] == score
+        self.assertEqual(len(incomplete), len(expected))
+        for video_idx, needs in expected:
+            actual = next(video for video in incomplete if video['path'] == videos[video_idx]['path'])
+            self.assertEqual(actual['video'].name, videos[video_idx]['name'])
+            self.assertEqual(len(actual['needs']), len(needs))
+            for lang, score in needs:
+                actual_need = next(need for need in actual['needs'] if str(need['lang']) == lang)
+                self.assertEqual(actual_need['current_score'], score)
